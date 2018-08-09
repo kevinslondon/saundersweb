13c2b94cb737ee40dae0ebf2e875a644e17e5a23
Kevin Saunders <ksaunders@saunderswebsolutions.com>
2018-08-09 17:24:50 +0100

    Create email template

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,6 +10,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Swift_Mailer;
+use Swift_Message;
 
 class DefaultController extends Controller
 {
@@ -26,9 +28,10 @@ class DefaultController extends Controller
     /**
      * @Route("/contact", name="contact")
      * @var $request Request
+     * @var
      * @return Response
      */
-    public function contactAction(Request $request)
+    public function contactAction(Request $request, Swift_Mailer $mailer)
     {
         // creates a task and gives it some dummy data for this example
         $task = new ContactEmail();
@@ -47,11 +50,16 @@ class DefaultController extends Controller
         if ($form->isSubmitted() && $form->isValid()) {
             // $form->getData() holds the submitted values
             // but, the original `$task` variable has also been updated
+            /**
+             * @var ContactEmail
+             */
             $task = $form->getData();
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($task);
             $entityManager->flush();
+
+            $this->send_contact_email($task->getName(), $task->getEmail(),$task->getPhone(), $task->getComments(), $mailer);
 
             return $this->redirectToRoute('contact_success');
         }
@@ -59,6 +67,34 @@ class DefaultController extends Controller
         return $this->render('contact.html.twig', array(
             'form' => $form->createView(),
         ));
+    }
+
+
+    private function send_contact_email($name, $email, $phone, $message, Swift_Mailer $mailer)
+    {
+        $params = compact($name,$email,$phone, $message);
+        $message = (new \Swift_Message('Contact from Saunders Web Solutions'))
+            ->setFrom('send@example.com')
+            ->setTo($email)
+            ->setBody(
+                $this->renderView(
+                    'emails/contact.html.twig',
+                    $params
+                ),
+                'text/html'
+            )
+
+            ->addPart(
+                $this->renderView(
+                    'emails/contact.txt.twig',
+                    $params
+                ),
+                'text/plain'
+            )
+
+        ;
+
+        $mailer->send($message);
     }
 
 }
